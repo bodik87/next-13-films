a758b8c2df44316067c49f490b5828e085be6a0e
Bogdan <bsshul@gmail.com>
2023-01-27 09:27:42 +0200

    Key

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,14 @@ import Movie from "./Movie";
 
 export default async function Home() {
   const data = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+    `https://api.themoviedb.org/3/movie/popular?api_key=9da82be405d4c60581aa5478eac5a945`
   );
   const res = await data.json();
   return (
     <main>
       <h1 className="text-3xl font-bold text-red-500 my-4">The MOVIES</h1>
       <div className="grid gap-8 grid-cols-fluid">
-        {res.results.map((movie) => (
+        {res?.results.map((movie) => (
           <Movie
             key={movie.id}
             id={movie.id}
